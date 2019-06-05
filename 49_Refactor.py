251fb384b35c7dda3c24436c83d03cb63ab88295
Derek <49753517+equitymarkets@users.noreply.github.com>
2019-06-05 17:53:10 +0300

    Update 49_Refactor.py

--- 49_Refactor.py
+++ 49_Refactor.py
@@ -1,8 +1,5 @@
-#Day 48: Days 25-48 Refactored Part 1 (will continue tomorrow)
-#For Days 25-47, I have coded data visualizations in Matplotlib, Numpy, Pandas,  
-#and Seaborn. Today I will clean them up.
-
-
+#Day 49: Days 25-47 written in functions and refactored and/or improved in 
+#most places
 
 import matplotlib.pyplot as plt											
 import numpy as np														
@@ -16,7 +13,7 @@ from pylab import *
 from random import seed					
 from random import randint 
 import statistics
-input()
+
 x = [0,1,2,3,4,5,6,7,8,9,10,11]											
 y = [3.94,3.50,3.72,3.51,3.56,3.63,3.67,3.75,3.77,3.62,3.57,3.53]
 bins = [3.50,3.55,3.60,3.65,3.70,3.75,3.80,3.85,3.90,3.95]
@@ -129,7 +126,8 @@ def variousmatplot():                       #Day 29
     market_shareType = [mobile, desktop]
     colorsType = ['lightblue','lightgreen']
 
-    wedgeoutline ={"edgecolor": "black","linewidth": 1,"linestyle": "solid", "antialiased": True}
+    wedgeoutline ={"edgecolor": "black","linewidth": 1,"linestyle": "solid", 
+    "antialiased": True}
 
     plots = arange(0.0,17.0,1)                                              
 
@@ -137,63 +135,68 @@ def variousmatplot():                       #Day 29
 
     histogram = subplot(2,2,1)                                              
     bins = [10,20,30,40,50]
-    plt.hist(market_share,bins,edgecolor='black',histtype='bar',rwidth=1,color='limegreen')
+    plt.hist(market_share,bins,edgecolor='black',
+    histtype='bar',rwidth=1,color='limegreen')
     plt.xlabel("Market Share")
     plt.ylabel("Frequency")
     plt.title('Histogram')
     
     pie = subplot(2,2,2)                                                    
-    plt.pie(market_share,wedgeprops=wedgeoutline,labels=OSlabels,colors=colors,autopct='%1.1f',startangle=135)
+    plt.pie(market_share,wedgeprops=wedgeoutline,labels=OSlabels,
+    colors=colors,autopct='%1.1f',startangle=135)
     plt.axis('equal')
     plt.title('Market Share by OS and Type')
 
     subplot(2,2,3)                                                          
     plt.figure("OS Use by %, 2019")
     bins = [10,20,30,40,50]
-    plt.hist(market_share,bins,edgecolor='black',histtype='bar',rwidth=1,color='limegreen',orientation='horizontal')
+    plt.hist(market_share,bins,edgecolor='black',histtype='bar',
+    rwidth=1,color='limegreen',orientation='horizontal')
     plt.xlabel("Market Share")
     plt.ylabel("Frequency")
 
     subplot(2,2,4)                                                          
-    plt.pie(market_shareType,wedgeprops=wedgeoutline,labels=TYPElabels,colors=colorsType,autopct='%1.1f',startangle=135)
+    plt.pie(market_shareType,wedgeprops=wedgeoutline,labels=TYPElabels,
+    colors=colorsType,autopct='%1.1f',startangle=135)
     plt.axis('equal')
     plt.tight_layout()
     plt.axis('equal')
     plt.show()
 
 
-def statisexport():                         #Day 30 *reading index values
+def statisexport():                         #Day 30 
 				
     input("Press enter to calculate of list of 10 random numbers.")
 	
-    ran_set = {}
-    keys = range(1,11)
-    x = []
-    for i in keys:					
-        x.append(i)
-        ran_set[i] = randint(1,1000)
-		
-    print(ran_set)							
-		
-    df = DataFrame(ran_set, index=[2])
+    ran_set = []
+
+    for i in range(1,11):					#random numbers
+        val = randint(1, 1000)
+        ran_set.append(val)
+
+    
+    print(ran_set)							#set of random numbers
+    df = DataFrame(ran_set)
     Day_30 = df.to_csv(r'C:\Users\D\python_work\Day_30.csv')
     input("Now, let's see some statistics about this set.")
-		
-    raw_data = sorted(ran_set)				
-    print(x)
-    print(ran_set)
-    print(df)
-				
+    
+    raw_data = sorted(ran_set)				#sorted set least to greatest
+    x = [1,2,3,4,5,6,7,8,9,10]
+    print(raw_data)
+    
     maximum = max(raw_data)
     minimum = min(raw_data)
-    rang = maximum - minimum				
+    rang = maximum - minimum				#range
     avg = statistics.mean(raw_data)
     med = statistics.median(raw_data)
     vari = statistics.variance(raw_data)
     std = statistics.stdev(raw_data)
-	
+
     input("This set of random numbers has a range of " + str(rang) + ", a mean of " + str(avg) + ", a median of " + str(med) + 
-    ", a variance of " + str(round(vari, 2)) + ", and a standard deviation of " + str(round(std, 2)) + ".") 
+    ", a variance of " + str(round(vari, 2)) + ", and a standard deviation of " + str(round(std, 2)) + ".")
+    plt.figure('Some Random Numbers from 1-1000')
+    plt.plot(x, ran_set,label='Numbers from 1-1000',linewidth='4',color='indigo')
+    plt.show() 
 
 
 def pandaimport():                          #Day 31
@@ -211,9 +214,12 @@ def pandaimport():                          #Day 31
     print(df.tail(10))
     input()
     inpt = input("Now, who would you like to get information for?")
-    outpt = df.loc[inpt]
-    query = DataFrame(outpt)
-    query.to_csv('Pokemon_Query.csv')
+    if inpt == "":
+        outpt = ("Thanks for visiting.")
+    else:
+        outpt = df.loc[inpt]
+        query = DataFrame(outpt)
+        query.to_csv('Pokemon_Query.csv')
     print(outpt)
 
 
@@ -231,7 +237,7 @@ def coffeeshop():                           #Days 32-33
     plt.show()                                                            
 
 
-def ASEAN():                                #Days 34-35 *unicode bytes error
+def ASEAN():                                #Days 34-35 
     raw_data = pd.read_csv('ASEAN.csv')
 
     countries = raw_data.iloc[:, 0]
@@ -243,11 +249,216 @@ def ASEAN():                                #Days 34-35 *unicode bytes error
 
     plt.figure("Domestic Import/Export ASEAN 2017")
     plt.title("ASEAN Domestic Import/Export 2017")
-    import_export = plt.bar(x = n, height = imports, color = 'fuchsia', width = .40)
-    export_import = plt.bar(x = n, height = exports, color = 'mediumspringgreen', width = .40, bottom = imports)
+    import_export = plt.bar(x = n, height = imports, color = 'fuchsia', 
+    width = .40)
+    export_import = plt.bar(x = n, height = exports, 
+    color = 'mediumspringgreen', width = .40, bottom = imports)
     plt.ylabel("Billions of USD")
     plt.xticks(n, countries, rotation = 45)
     plt.legend([import_export, export_import], ('Imports', 'Exports'))
+    plt.tight_layout()
+    #plt.adjust(hspace=.3)
+    plt.show()
+
+
+def doppler():                              #Days 36-37
+    print("Light waves tending towards the red end of the visible spectrum ") 
+    print("have lower frequency than those near the blue end of the spectrum.") 
+    print("This, considering cosmic proportions of scale, results in the fact")
+    print("that galaxies moving away from us (most) will appear red to the ")
+    print("human eye, whereas galaxies moving towards us (Andomeda for ")
+    print("instance) will appear blue. The same effect occurs when measuring")
+    print("sound.")
+    
+    input("Press enter to see why.")
+
+    plt.figure('Doppler Effect')                            
+    
+    red = np.arange(0, 10, .1)
+    blue = np.arange(0, 20, .1)
+    ramp = np.sin(red)
+    bamp = np.sin(blue)
+
+    red_shift = plt.subplot(2,1,1)                  
+    plt.title('Red Shift / Blue Shift')
+    plt.ylabel('Amplitude')
+    plt.annotate('', xy=(6,.5), xytext = (4,.5), 
+    arrowprops=dict(arrowstyle="->")) 
+    plt.plot(red, ramp, color='crimson')
+    plt.axis('off')
+    plt.axhline(y=0, color='black')
+
+    blue_shift = plt.subplot(2,1,2)                 
+    plt.xlabel('Wavelength')
+    plt.ylabel('Amplitude')
+    plt.annotate('', xy=(8,.5), xytext = (12,.5), 
+    arrowprops=dict(arrowstyle="->"))
+    plt.plot(blue, bamp, color='dodgerblue')
+    plt.axis('off')
+    plt.axhline(y=0, color='black')
+    plt.show()                                      
+
+
+def appliedfunctions():                     #Days 38, 39, and 40
+    raw_data = pd.read_csv("country_population.csv")
+
+    population1 = raw_data.sort_values('2016')
+    sort_one = population1.to_csv(r'C:\Users\D\python_work\sort1.csv')
+    
+    population2 = raw_data.sort_values('2016', ascending=False)
+    sort_two = population2.to_csv(r'C:\Users\D\python_work\sort2.csv')
+    
+    population3 = raw_data.sort_values('2016',na_position='first')
+    sort_three = population3.to_csv(r'C:\Users\D\python_work\sort3.csv')
+    
+    population4 = raw_data.sort_values(['1960','2016'])
+    sort_four = population4.to_csv(r'C:\Users\D\python_work\sort4.csv')
+
+
+def seaborn():                              #Day 41
+    df = pd.read_csv("country_population.csv")
+    plt.figure("Country Population")
+    plt.subplot(2,2,1)
+    sns.set_style('darkgrid')
+    one = sns.scatterplot(x='2016',y='1960',data=df, color='mediumvioletred')
+    
+    
+    plt.subplot(2,2,2)
+    sns.set_style('whitegrid')
+    two = sns.barplot(x='2016',y='1960',data=df, color='darkorchid')
+    
+    
+    plt.subplot(2,2,3)
+    sns.set_style('dark')
+    three = sns.lineplot(x='2016',y='1960', data=df, color='dodgerblue')
+    
+    plt.subplot(2,2,4)
+    sns.set_style('white')
+    four = sns.boxplot(x='2016',y='1960', data=df, color='chartreuse')
+    
+    plt.show()
+
+
+def heatmap():                              #Days 42 and 43
+    
+    ans = sns.load_dataset('anscombe')
+    anspiv = ans.pivot('dataset', 'y', 'x')
+    heat = sns.heatmap(anspiv)
+    
+    data = ans.pop('dataset')
+    cluster = sns.clustermap(ans)
+    plt.show()
+    
+
+def joint():                                #Day 44
+    example_four = sns.load_dataset('flights')
+    sns.set(style='darkgrid')
+    im = sns.load_dataset('flights')
+    sns.jointplot(x = 'year',y = 'passengers', data=im, kind="hex",color="lime")
+
+    plt.show()    
+ 
+ 
+def boxdist():                              #Day 45
+    dot = sns.load_dataset('dots')
+    plt.figure('Some Data From Dots.csv')
+    plt.subplot(2,2,1)                                              
+    sns.boxplot(x=dot['choice'],y=dot['coherence'],color='mediumorchid')
+
+    plt.subplot(2,2,2)                                              
+    sns.boxplot(x=dot['align'],y=dot['firing_rate'],color='lightgreen')
+
+    plt.subplot(2,2,3)                                              
+    sns.distplot(dot['coherence'],color='deeppink')
+
+    plt.subplot(2,2,4)                                               
+    sns.distplot(dot['firing_rate'],color='greenyellow')
+    plt.show()   
+    
+
+def swarm():                                #Day 46
+    df_import = pd.read_csv('planets.csv')                                                        
+    plt.figure("Planets Spreadsheet")
+    sns.swarmplot(x="year",y="distance", data=df_import)
+    plt.xticks(rotation=75)
+    plt.show()
+
+
+def many():                                 #Day 47
+    exercise_data = pd.read_csv("exercise.csv")         #exercise spreadsheet
+
+    #LMPlot
+    lmplot = sns.lmplot(x='id',y='pulse',data=exercise_data)
+    
+    #BarPlot
+    plt.figure("Barplot")
+    barplot = sns.barplot(x='id',y='pulse',data=exercise_data)
+    plt.xticks(size=8)
+    
+    #KDEPlot
+    plt.figure()
+    x = exercise_data.pulse
+    KDE = sns.kdeplot(x, shade=True, color='green')
+    
+    #ScatterPlot
+    plt.figure()
+    scatterplot=sns.scatterplot(x='pulse',y='time',data=exercise_data, 
+    color='purple')
+    
+    #DistPlot
+    plt.figure()
+    a = exercise_data.pulse
+    distplot = sns.distplot(a, color='limegreen')
+    
+    #LinePlot
+    plt.figure()
+    lineplot = sns.lineplot(x='pulse',y='id',data=exercise_data, color='red')
+    
+    #RelPlot
+    relplot = sns.relplot(x='pulse',y='id',data=exercise_data)
+    
+    #CatPlot
+    catplot = sns.catplot(x='diet',y='id',hue='pulse',data=exercise_data)
+    
+    #BoxPlot
+    plt.figure()
+    boxplot = sns.boxplot(x='diet',y='pulse',data=exercise_data)
+    
+    #ViolinPlot
+    plt.figure()
+    violin = sns.violinplot(x='diet',y='pulse',hue='id', inner='quart', 
+    data=exercise_data)
+    
+    #HeatMap
+    plt.figure()
+    p_e_data = exercise_data.pivot('time','id','pulse')
+    exercise_heatmap = sns.heatmap(p_e_data)
+    
+    #JointPlot
+    sns.set(style='white')
+    jointplot = sns.jointplot(x='pulse',y='id',data=exercise_data, 
+    kind='hex', color='darkblue')
+    
+    #StripPlot
+    plt.figure()
+    stripplot = sns.stripplot(x='id',y='pulse',data=exercise_data)
+    
+    #BoxenPlot
+    plt.figure()
+    boxen = sns.boxenplot(x='id',y='time',data=exercise_data,scale='exponential')
+    
+    #ResidPlot
+    plt.figure()
+    residplot = sns.residplot(x='id',y='pulse',data=exercise_data)
+    
+    #SwarmPlot
+    plt.figure("Swarm Plot")
+    swarm = sns.swarmplot(x='pulse',y='time',data=exercise_data)
+    
+    #PairPlot
+    inp = exercise_data
+    pairplot = sns.pairplot(inp)
+    
     plt.show()
 
 
@@ -259,13 +470,26 @@ supplydemand()
 
 variousmatplot()
 
-#statisexport()
+statisexport()
 
 pandaimport()
 
 coffeeshop()
 
-#ASEAN()
+ASEAN()
 
+doppler()
 
+appliedfunctions()
 
+seaborn()
+
+heatmap()
+
+joint()
+
+boxdist()
+
+swarm()
+
+many()
