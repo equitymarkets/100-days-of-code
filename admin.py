e40dc8833a76570e5f654de97129fa234e0e07ad
Derek <49753517+equitymarkets@users.noreply.github.com>
2019-06-20 18:45:10 +0300

    Add files via upload

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from forest.models import Objective
+from forest.models import Goal, Description
 
-admin.site.register(Objective)
-
+admin.site.register(Goal)
+admin.site.register(Description)
--- models.py
+++ models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
-
-
 # Create your models here.
-class Objective(models.Model):
-    OFFER = 'OF'
-    SEARCH = 'SC'
-    offer_or_searchfor = [(OFFER, 'offer'), (SEARCH, 'search')]
-    need = models.CharField(max_length=2, choices=offer_or_searchfor, 
-        default=OFFER)
-    date = models.DateTimeField(auto_now_add=True)
+class Goal(models.Model):
+    """I plan to define project goals here by time"""
+    text = models.CharField(max_length=15)
+    date_added = models.DateTimeField(auto_now_add=True)
     
-def __str__(self):
-    return self.text
+    def __str__(self):
+        """Return the string to page"""
+        return self.text
+
+class Description(models.Model):
+    """Description of goal and projected date of completion"""
+    topic = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """Returns the description of the goal"""
+        return self.text[:100] + "..."
