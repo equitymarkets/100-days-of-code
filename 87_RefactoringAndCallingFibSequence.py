a8e927b957665986e95fec42b28f8915d4238208
Derek <49753517+equitymarkets@users.noreply.github.com>
2019-07-17 19:23:18 +0300

    Create 87_RefactoringAndCallingFibSequence.py

---
+++ 87_RefactoringAndCallingFibSequence.py
@@ -0,0 +1,12 @@
+#Day 87: Exploring the internals of Python...
+#Milestones: importing my own work, understanding .py = module
+#(A module is simply a .py file that can be imported)
+
+import Fib as F
+import matplotlib.pyplot as plt											
+import numpy as np
+
+
+F.Fib_Func()
+
+print(F.Fib_Func())
