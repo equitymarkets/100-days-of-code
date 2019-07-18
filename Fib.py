d9de7cc443a988ea8ea609a569c5e1f107a90782
Derek <49753517+equitymarkets@users.noreply.github.com>
2019-07-18 17:42:14 +0300

    Add files via upload

--- Fib.py
+++ Fib.py
@@ -4,33 +4,42 @@
 def Fib_Func():
     
     
-    number = input("Please enter a number ")
-    		
-    A = 0										
-    B = 1
-    C = 1
-    D = B + C 
-    E_array = []
+    numb = int(input("How many times would you like to run the cycle?"))
+    
+    
+    
+    while numb > 0:
+        
+        number = input("Please enter a number ")
+        
+        A = 0										
+        B = 1
+        C = 1
+        D = B + C 
+        E_array = []
+    
+        print(B)									#1
+        
+        print(C)									#1
+        
+        print(D)									#2
+        
+    
 
-    print(B)									#1
-    E_array.append(B)
-    print(C)									#1
-    E_array.append(C)
-    print(D)									#2
-    E_array.append(D)
 
 
-
-
-    while D < int(number):
-        A = B
-        B = C
-        C = D
-        D = C + B
-        print(D)
-        E_array.append(D)
-
+        while D < int(number):
+            A = B
+            B = C
+            C = D
+            D = C + B
+            print(D)
+            E_array.append(float(D/C))
+            
+        numb -= 1
+        
     return(E_array)
-
-	
-
+    
+        
+    
+    
