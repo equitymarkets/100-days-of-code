8e15219fb65d1ecce912c75e9238dd2a06e4c18d
Derek <49753517+equitymarkets@users.noreply.github.com>
2020-05-05 11:13:18 +0300

    Create 066R2_day66.js

---
+++ 066R2_day66.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+              $('div.top-block').fadeIn(2000, function() {
+                  $('div.two-block').fadeIn(2000, function() {
+                      $('div.three-block').fadeIn(2000);
+                    });
+                });
+            });
